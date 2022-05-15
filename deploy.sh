5643d65e0bcf95ffb75da57b3f63088ed2873290
Hibai Unzueta <hibai.unzueta@gmail.com>
2022-05-15 22:19:38 +0100

    Setup

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/hanbzu/questions main:gh-pages
+git push -f https://github.com/hanbzu/questions.git main:gh-pages
+#git subtree push --prefix dist origin gh-pages
 
 cd -
